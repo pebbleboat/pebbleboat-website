4510c395b23bb9e850f76bc8d0c58ba52da3d525
Suryansh Shahi <suryanshshahi@Suryanshs-MacBook-Air.local>
2026-05-20 23:20:29 +0530

    added gtm

--- app/components/GoogleAnalytics.tsx
+++ app/components/GoogleAnalytics.tsx
@@ -1,7 +1,7 @@
 import { GoogleAnalytics as NextGoogleAnalytics } from "@next/third-parties/google";
 
 const GA_MEASUREMENT_ID =
-  process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || "G-W41LQX67EL";
+  process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || "G-GG1XMCCLMR";
 
 export default function GoogleAnalytics() {
   if (!GA_MEASUREMENT_ID) return null;
